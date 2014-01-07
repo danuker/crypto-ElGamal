0c5260da6e32f4dcf02638462f50c9db4cb8d2d7
Kreubs <bnibbe3@yahoo.com>
2014-01-07 16:18:23 +0200

    encryption, valid
    added message validation and finished encryption

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from random import randint          # For choosing large numbers
         # Find prime number (keep using Miller-Raabin on every number) (CHECK)
         # Find generator
 
+alphabet = "abcdefghijklmnopqrstuvwzy "
+
 def get_keys(bits):
     '''
     Gets key
@@ -25,12 +27,23 @@ def get_keys(bits):
     
     while(1):
         a = get_prime(a_bits)
+        break
     
     ga = pow(g, a, p)
     
     return {"public":(p, g, ga), "private":a}
 
+def valid(message):
+    for c in message:
+        if c not in alphabet:
+            return False
+
+    return True
+
 def encrypt(public_key, message):
+    if not valid(message):
+        raise Exception("Invalid message")
+    
     p, g, ga = public_key
     k = randint(1, p)
     alpha = pow(g, k, p)
@@ -39,6 +52,16 @@ def encrypt(public_key, message):
     if len(message) & 1:
         message += " "
 
-    
+    ciphertext = []
+    for i in range(0, len(message), 2):
+        l1, l2 = message[i], message[i+1]
+        m1, m2 = string.index(l1), string.index(l2)
+        m = 27 * m1 + m2
+        beta = m * pow(ga, k, p)
+        ciphertext.append((alpha,beta))
 
-    
+    return ciphertext
+
+
+
+        
