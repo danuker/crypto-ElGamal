dbfefe2c97e2cfb992614d6981d93018f39abef9
Kreubs <bnibbe3@yahoo.com>
2014-01-08 14:33:40 +0200

    more awesome testing

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 from primality import get_prime
 from main import *
 
+message = 'this is a new message'
+
 for i in range(10):
     keys = get_keys(256) # Ask about what happens <16 bits
     #keys = {'public': (163L, 23L, 125L), 'private': 131L}
 
-    cipher = encrypt(keys['public'], 'this is a longer message than before')
+    cipher = encrypt(keys['public'], message)
     plain = decrypt(keys, cipher)
 
-    print keys
-    print plain
+    if plain != message:
+        print keys
+        print plain
+
+print "done"
