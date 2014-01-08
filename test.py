b28b3e3a417f64d27521d887627b58f88bc3ae35
Kreubs <bnibbe3@yahoo.com>
2014-01-08 13:25:54 +0200

    added loop for testing

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
- 
 from primality import get_prime
 from main import *
 
-keys = get_keys(256) # Ask about what happens <16 bits
-#keys = {'public': (163L, 23L, 125L), 'private': 131L}
+for i in range(10):
+    keys = get_keys(256) # Ask about what happens <16 bits
+    #keys = {'public': (163L, 23L, 125L), 'private': 131L}
 
-cipher = encrypt(keys['public'], 'message')
-plain = decrypt(keys, cipher)
+    cipher = encrypt(keys['public'], 'this is a longer message than before')
+    plain = decrypt(keys, cipher)
 
-print keys
-print plain
+    print keys
+    print plain
